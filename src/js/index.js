413caea3a1ec8773034d1b8f778adbf4044f4873
JungSik Jeong <jungsik.jeong@lcnine.kr>
2025-02-11 22:47:42 +0900

    화면출력 (애니메이션,반응형)

--- src/js/index.js
+++ src/js/index.js
@@ -31,5 +31,22 @@ const material = new THREE.MeshStandardMaterial({ color: 0x2e6ff2 });
 const box = new THREE.Mesh(geometry, material);
 scene.add(box);
 
-// 4. 렌더링
-renderer.render(scene, camera);
+function animate() {
+  box.rotation.y += 0.01;
+  requestAnimationFrame(animate); // 브라우저가 다음 프레임을 렌더링하기전에 반복적으로 호출
+
+  renderer.render(scene, camera);
+}
+
+animate();
+
+window.addEventListener('resize', () => {
+  renderer.setSize($result.clientWidth, $result.clientHeight);
+
+  // 1. 카메라 종횡비
+  camera.aspect = $result.clientWidth / $result.clientHeight;
+  camera.updateProjectionMatrix(); // 카메라 업데이트
+
+  // 2. 렌더러의 크기
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
